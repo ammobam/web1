fe7c0d48df7741dd1463420a3f316e1bcfd72ecf
Hyejin <55999351+ammobam@users.noreply.github.com>
2020-12-19 23:52:18 +0900

    Add files via upload

---
+++ justdoit.php
@@ -0,0 +1,29 @@
+<!DOCKTYPE html>
+<html>
+<head>
+<h1>구조</h1><br>
+
+<style>
+.myDiv {
+border: 5px outset gold;
+background-color: lightblue;
+text-align: center;
+width=500
+}
+
+</style>
+</head>
+<body>
+
+<div class="myDiv">
+<h2>참고하면 좋을 사이트!</h2>
+<p>https://www.w3schools.com/tags/tag_html.asp</p>
+</div>
+</body>
+<html>
+<p>
+<img src="https://cdn.pixabay.com/photo/2019/08/26/15/59/chipmunk-4432122_1280.jpg"
+width="70%"
+vertical-align: middle; margin:100px 0px
+class="center">
+</p>
