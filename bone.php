52498c43adb7c58767d371b9b4033b64c23249b8
Hyejin <55999351+ammobam@users.noreply.github.com>
2020-12-22 23:55:04 +0900

    Create bone.php

---
+++ bone.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="kr">
+<head>
+    <meta charset="utf-8">
+<title>Portfolio Web</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <div>
+    <h1>Portfolio Web</h1>
+    </div>
+
+    <div>
+    <img scr="">
+    <h3>Profile</h3>
+    <ul>
+    <li><a href="">Contact</a></li>
+    <li><a href="">Skills</a></li>
+    </ul>
+    </div>
+
+    <div>
+    <?php
+<!--
+방문자수 카운트
+-->
+     ?>
+    </div>
+
+</body>
+
+</html>
